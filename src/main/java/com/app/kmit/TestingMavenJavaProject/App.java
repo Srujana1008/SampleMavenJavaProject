be31cf9acf73b3fe4cab12238b4173c6a8a68018
Srujana1008 <srujanagoje1008@gmail.com>
2024-11-17 21:02:25 +0530

    Sample Maven Java

---
+++ src/main/java/com/app/kmit/TestingMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.app.kmit.TestingMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Srujana 22BD1A662M" );
+    }
+}
